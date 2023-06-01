1c74a39580c8c5856ba4d17786ebba4074c2f727
Queenie <queenieb8me@gmail.com>
2023-05-31 21:53:56 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,25 +5,27 @@ import "./App.css";
 export default function App() {
 	return (
 		<div className="App">
-			<h1>Weather App </h1>
-			<footer>
-				This project was coded by{" "}
-				<a
-					href="https://meek-lebkuchen-5e6974.netlify.app/about.html"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Queenie Navarro
-				</a>{" "}
-				and is {""}
-				<a
-					href="https://github.com/qnavarro/my-react-app"
-					target="_blank"
-					rel="noreferrer"
-				>
-					open sourced on GitHub
-				</a>
-			</footer>
+			<div className="container">
+				<h1>Weather App </h1>
+				<footer>
+					This project was coded by{" "}
+					<a
+						href="https://meek-lebkuchen-5e6974.netlify.app/about.html"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Queenie Navarro
+					</a>{" "}
+					and is {""}
+					<a
+						href="https://github.com/qnavarro/my-react-app"
+						target="_blank"
+						rel="noreferrer"
+					>
+						open sourced on GitHub
+					</a>
+				</footer>
+			</div>
 		</div>
 	);
 }
