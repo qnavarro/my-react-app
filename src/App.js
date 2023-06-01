d9c60717ca2f6b89c1e6228eb2141d8b9edc0d1d
Queenie <queenieb8me@gmail.com>
2023-05-31 21:28:14 -0700

    added git repo url

--- src/App.js
+++ src/App.js
@@ -3,5 +3,27 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-	return <div className="App"> Weather App</div>;
+	return (
+		<div className="App">
+			<h1>Weather App </h1>
+			<footer>
+				This project was coded by{" "}
+				<a
+					href="https://meek-lebkuchen-5e6974.netlify.app/about.html"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Queenie Navarro
+				</a>{" "}
+				and is {""}
+				<a
+					href="https://github.com/qnavarro/my-react-app"
+					target="_blank"
+					rel="noreferrer"
+				>
+					open sourced on GitHub
+				</a>
+			</footer>
+		</div>
+	);
 }
