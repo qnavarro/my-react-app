101568e6a32632440e143ed4ff95daec5c15f7ad
Queenie <queenieb8me@gmail.com>
2023-06-01 23:45:03 -0700

    Included Props

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
 	const [weatherData, setWeatherData] = useState({ ready: false });
 	function handleResponse(response) {
 		console.log(response.data);
@@ -75,8 +75,8 @@ export default function Weather() {
 		);
 	} else {
 		const apiKey = "a002c7ba4b8d6faff3c087bcf141fto4";
-		let city = "Los Angeles";
-		let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+		let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
 		axios.get(apiUrl).then(handleResponse);
 
 		return "Loading...";
