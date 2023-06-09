7baef3729eac2b9f3734e674ac0b74c0a1e1a7f8
Queenie <queenieb8me@gmail.com>
2023-06-08 23:42:44 -0700

    Added in shecodes api

---
+++ src/WeatherForecast.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./WeatherForecast.css";
+import axios from "axios";
+
+export default function WeatherForecast(props) {
+	function handleResponse(response) {
+		console.log(response.data);
+	}
+	let apiKey = `a002c7ba4b8d6faff3c087bcf141fto4`;
+	let lon = 70;
+	let lat = 90;
+	let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}`;
+
+	axios.get(apiUrl).then(handleResponse);
+
+	return (
+		<div className="WeatherForecast">
+			<div className="row">
+				<div className="col">
+					<div className="WeatherForecast-day">Thu</div>
+					<img
+						src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+						alt="clear-sky"
+					/>
+					<div className="WeatherForecast-temperatures">
+						{" "}
+						<span className="WeatherForecast-temperature-max">19°</span>
+						<span className="WeatherForecast-temperature-min">10°</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
