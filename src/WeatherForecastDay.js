d56a3f8e9047687e9a0a3bce34737397b47f9cf7
Queenie <queenieb8me@gmail.com>
2023-06-11 19:59:13 -0700

    code still broken but until help is received, must continue on with Matt's lessons

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,18 +1,30 @@
 import React from "react";
 
 export default function WeatherForecastDay(props) {
+	function maxTemp() {
+		let temp = Math.round(props.temperature.maximum);
+		return `${temp}°`;
+	}
+	function minTemp() {
+		let temp = Math.round(props.temperature.maximum);
+		return `${temp}°`;
+	}
+
+	function day() {
+		let date = new Date(props.time * 1000);
+		let day = date.getDay();
+		let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+		return days[day];
+	}
 	return (
 		<div>
-			<div className="WeatherForecast-day">{props.time}</div>
+			<div className="WeatherForecast-day">{day()}</div>
 			<img src={props.condition.icon_url} alt={props.condition.icon} />
 			<div className="WeatherForecast-temperatures">
 				{" "}
-				<span className="WeatherForecast-temperature-max">
-					{props.temperature.minimum}
-				</span>
-				<span className="WeatherForecast-temperature-min">
-					{props.temperature.maximum}
-				</span>
+				<span className="WeatherForecast-temperature-max">{maxTemp()}</span>
+				<span className="WeatherForecast-temperature-min">{minTemp()}</span>
 			</div>
 		</div>
 	);
